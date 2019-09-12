847d4ca75f744fd60b757baabe0fab731eb0c26f
EmersonJoseBomfimSampaio <40372739+EmersonJoseBomfimSampaio@users.noreply.github.com>
2019-09-12 15:38:32 -0300

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,51 @@
+package com.bestgroup.app.domain;
+
+import com.bestgroup.core.domain.Entity;
+
+public class Address extends Entity{
+	
+	protected String rua;
+	protected String complemento;
+	protected String numero;
+	protected String cidade;
+	protected String bairro;
+	protected String estado;
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+}
---
+++ Phone.java
@@ -0,0 +1,15 @@
+package com.bestgroup.app.domain;
+
+public class Phone {
+	
+	protected String number;
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+}
