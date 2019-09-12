9445236b52d27d4cfee2c0ca18060f4148fa48d4
Emerson Jose Bomfim Sampaio <emersonsampaio@brq.com>
2019-09-12 15:43:12 -0300

    Models

--- src/main/java/com/bestgroup/app/domain/Customer.java
+++ src/main/java/com/bestgroup/app/domain/Customer.java
@@ -1,5 +1,46 @@
 package com.bestgroup.app.domain;
 
+import java.util.Date;
+import java.util.List;
+
 public class Customer extends User {
+	
+	protected List<Address> addresses;
+	protected List<Phone> phones;
+	protected String gender;
+	protected String cpf;
+	protected String dataNasc;
+	
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+	public List<Phone> getPhones() {
+		return phones;
+	}
+	public void setPhones(List<Phone> phones) {
+		this.phones = phones;
+	}
 	
 }
